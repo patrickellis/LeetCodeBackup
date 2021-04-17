6e308084501f8d211e618d0facb8e47cb68fbf4b
Patrick Ellis <patrick.ellis.w@gmail.com>
2021-04-17 16:56:20 +0100

    1114 sol

---
+++ 1114.cpp
@@ -0,0 +1,38 @@
+class Foo {
+public:
+    mutex mtx;
+    condition_variable cv;
+    int count =0;
+    Foo() {
+        count=1;
+    }
+
+    void first(function<void()> printFirst) {
+        unique_lock<mutex> lck(mtx);
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        count=2;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex> lck(mtx);
+        while(count!=2){
+            cv.wait(lck);    
+        }
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        count=3;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        unique_lock<mutex> lck(mtx);
+        while(count!=3){
+            cv.wait(lck);
+        }
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
