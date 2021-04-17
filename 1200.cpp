ebe4b462bcf85498dca6e5ba5528b977417b2055
Patrick Ellis <patrick.ellis.w@gmail.com>
2021-04-17 16:57:27 +0100

    1200

---
+++ 1200.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        vector<vector<int>> res;
+        int min=INT_MAX;
+        sort(arr.begin(), arr.end());
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]-arr[i-1]<min)min=arr[i]-arr[i-1];
+        }
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]-arr[i-1]==min)res.push_back({arr[i-1],arr[i]});
+        }
+        return res;
+    }
+};
