15efe33aa4d48ea1fb013fae993fa55e6ec1b454
Patrick Ellis <patrick.ellis.w@gmail.com>
2021-04-19 19:41:37 +0100

    two sum:

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> M;
+        for(int i=0;i<nums.size();i++){
+            auto it = M.find(target-nums[i]);
+            if(it != M.end()){
+                return {it->second, i};
+            }
+            M[nums[i]]=i;
+        }
+        return {};
+    }
+};
