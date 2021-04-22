db31a11b17c057d3a931985fcbc6c90b1014ab60
Patrick Ellis <patrick.ellis.w@gmail.com>
2021-04-22 22:25:43 +0100

    235

---
+++ 235.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+#pragma GCC optimize("O3")
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val > p->val && root->val > q->val){
+            return lowestCommonAncestor(root->left,p,q);    
+        }
+        if(root->val < p->val && root->val < q->val){
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        return root;
+    }
+};
