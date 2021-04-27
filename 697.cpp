5bde0ec37e40f1e7e35fa4fdbbe54d768f16cc2c
Patrick Ellis <patrick.ellis.w@gmail.com>
2021-04-27 23:22:37 +0100

    697

---
+++ 697.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        unordered_map<int,int> count, first;
+        int res = 0, degree = 0;
+        for(int i=0;i<nums.size();i++){
+            if(first.count(nums[i]) == 0)first[nums[i]] = i;
+            if(++count[nums[i]] > degree){
+                degree = count[nums[i]];
+                res = i - first[nums[i]]+1;
+            }
+            else if(count[nums[i]]==degree){
+                res = min(res, i - first[nums[i]]+1);
+            }                
+        }
+        return res;
+    }
+};
