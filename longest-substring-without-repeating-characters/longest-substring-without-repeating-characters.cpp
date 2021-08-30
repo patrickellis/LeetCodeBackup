1782a3499d205e5d614922f5fb25e493295abb06
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-08-30 16:39:45 +0100

    Time: 8 ms (84.55%), Space: 6.9 MB (95.93%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int best=0,current=0;
-        int cnt[128];
-        int lastStart=0;
-        memset(cnt,-1,sizeof(cnt));
-        for(int i=0;i<s.size();i++){
-            if(cnt[s[i]]==-1){
-                cnt[s[i]]=i;
-                current++;
+        if(s.size()<=1) return s.size();
+        int dict[128];
+        int maxLen=0, start=-1;
+        memset(dict, -1, sizeof(dict));
+        
+        for(int i=0; i<s.size(); i++){
+            if(dict[s[i]] > start){
+                start=dict[s[i]];
             }
-            else{                
-                current=min(i-(cnt[s[i]]),i-lastStart);
-                cnt[s[i]]=i;
-                lastStart=i-current;
-            }
-            cout << "char: " << s[i] << ", current: " << current << "\n";
-            //cout << "i: " << i << ",cnt[s[i]]: " << i-cnt[s[i]]+1 << "\n";
-            best = max(best,current);
+            maxLen = max(maxLen, i-start);
+            dict[s[i]] = i;
         }
-        return best;
+        return maxLen;
     }
 };
