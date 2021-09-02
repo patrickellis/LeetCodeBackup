78610b16cb6d9669dcf3875e6598a59512d798a1
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-02 17:04:53 +0100

    Time: 78 ms (33.88%), Space: 48.3 MB (78.20%) - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxReach=0;
-        for(int jump = 0; jump < nums.size()-1; jump++){
-            if(maxReach < jump) return false;
-            maxReach = max(maxReach, jump+nums[jump]);
+        int len = 0;
+        for(auto i = 0; i < nums.size() && len >= i; i++){
+            len = max(len, i + nums[i]);
         }
-        cout << maxReach;
-        return maxReach >= nums.size()-1;        
+        return nums.size() <= len + 1;
     }
 };
