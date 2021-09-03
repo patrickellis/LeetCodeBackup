a0d304656901d36561daa09d06ba45d00d96ce1a
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-03 20:21:00 +0100

    Time: 3 ms (40.60%), Space: 6.6 MB (64.20%) - LeetHub

--- combination-sum-iv/combination-sum-iv.cpp
+++ combination-sum-iv/combination-sum-iv.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        long dp[target + 1];
-		long tmp;
-		dp[0] = 1;
-		sort(nums.begin(), nums.end());
-		for (int i = 1; i <= target; i++) {
-			dp[i] = 0;
-			for (int num: nums) {
-				if (i < num) break;
-				tmp = dp[i] + dp[i-num];
-				if (tmp >= INT_MAX) {
-					dp[i] = 0;
-					break;
-				}
-				dp[i] = tmp;
-			}
-		}
-		return (int) dp[target];
+        unsigned int dp[target+1];
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        for(int i = 1; i <= target; ++i){
+            for(int n : nums){
+                if(i >= n){
+                    dp[i] += dp[i-n];
+                }
+            }    
+        }
+        return dp[target];
     }
 };
