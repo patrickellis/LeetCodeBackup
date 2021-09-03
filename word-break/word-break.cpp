1246f3316e70463fcac01d72a5b75ebf190854a8
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-03 16:02:23 +0100

    Time: 7 ms (75.31%), Space: 9.4 MB (74.80%) - LeetHub

--- word-break/word-break.cpp
+++ word-break/word-break.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int sz = s.size();
-        // dp array -> dp[i] = is there a substring in dictionary that ends at pos i
         bool dp[sz+1];
         memset(dp, false, sizeof(dp));
-        dp[0] = true; 
-        
-        for(int i = 1; i <= sz; i++){
-            for(int j = i - 1; j >= 0; --j){
+        dp[0] = true;
+        for(int i = 1; i <= sz; ++i){
+            for(int j = i-1; j >= 0; --j){
                 if(dp[j]){
                     auto ss = s.substr(j, i-j);
-                    if(find(wordDict.begin(), wordDict.end(), ss) != wordDict.end()){
+                    if(find(wordDict.begin(), wordDict.end(),ss) != wordDict.end()){
                         dp[i] = true;
                         break;
                     }
                 }    
             }
-        }
+                        
+        }        
         return dp[sz];
     }
 };
