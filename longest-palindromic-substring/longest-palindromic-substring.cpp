34594304670e6294930824b8a3fb87039bbe8a6b
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-03 16:15:32 +0100

    Time: 735 ms (9.99%), Space: 385.6 MB (10.56%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        // base case, empty string
-        if(n==0) return "";
-        // size 1, return only character in string
-        if(n==1) return s;
+        int sz = s.size();
+        if(sz == 1) return s;
         
-        // dp array -> dp[i][j] = substring s(i,j) is a palindrome
-        bool dp[n][n];
-        memset(dp,0,sizeof(dp));
+        // dp array -> dp[i][j] = substring (i,j) is a palindrome
+        vector<vector<int>> dp(sz, vector<int>(sz,false));
         
-        for(int i=0;i<n;i++)
-            dp[i][i]=true; // all substring length 1 are palindromes
+        for(int i = 0; i < sz; ++i)
+            dp[i][i] = true; // all substrings (i,i) are palindromic
         
-        // initialise ans with first character,
-        // we know this is a valid palindrome of length 1
-        string ans = "";
-        ans+=s[0];
+        int maxLen = 0;
+        string result = "";
+        result += s[0];
         
-        for(int i=n-1;i>=0;i--){
-            for(int j=i+1;j<n;j++){
-                if(s[i]==s[j]){
-                   if(j-i==1 || dp[i+1][j-1]){
-                        dp[i][j]=true;
-                        if(j-i+1 > ans.size())
-                            ans = s.substr(i,j-i+1);
-                    } 
-                }
+        for(int i = sz-1; i >= 0; --i){
+            for(int j = i + 1; j < sz; ++j){
+                if(j - i == 1 || dp[i+1][j-1]){
+                    if(s[i] == s[j]){
+                        dp[i][j] = true;
+                        if(j - i + 1 > result.size()) 
+                            result = s.substr(i,j - i + 1);
+                    }
+                }    
                 
             }    
         }
-        return ans;
+        return result;
     }
 };
