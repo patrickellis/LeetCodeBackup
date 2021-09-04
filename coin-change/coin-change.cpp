88717b61a7a5311644c198aac93449ef28962715
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-04 14:44:00 +0100

    Time: 35 ms (96.45%), Space: 10 MB (90.51%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        long long dp[amount+1];
-        memset(dp, 0, sizeof(dp));
-        dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            dp[i]=INT_MAX;
-            for(auto& c:coins){
-                if(i-c >= 0) dp[i] = min(dp[i-c]+1, dp[i]);
-            }
+        unsigned int dp[amount+1];
+        dp[0] = 0;
+        for(int i = 1; i <= amount; ++i){
+            dp[i] = INT_MAX;
+            for(auto c:coins){
+                if(c <= i) dp[i] = min(dp[i],dp[i-c]+1);
+            }    
         }
         return dp[amount]<INT_MAX?dp[amount]:-1;
     }
