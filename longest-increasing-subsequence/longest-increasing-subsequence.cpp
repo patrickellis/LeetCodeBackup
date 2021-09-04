7e63c705b527c9fa1c739855e787ec36cabe0fa3
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-04 15:13:06 +0100

    Time: 329 ms (22.36%), Space: 10.3 MB (97.27%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& arr) {
-    int n = arr.size();
-    int lis[n];
- 
-    lis[0] = 1;
- 
-    /* Compute optimized LIS values in
-       bottom up manner */
-    for (int i = 1; i < n; i++) {
-        lis[i] = 1;
-        for (int j = 0; j < i; j++)
-            if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
-                lis[i] = lis[j] + 1;
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size(); 
+        int dp[n]; // dp[i] = longest subsequence that ends at index i;
+        for(int i = 0; i < n; ++i) dp[i] = 1;
+        
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < i; ++j){
+                if(nums[i] > nums[j] && dp[i] < dp[j]+1)
+                    dp[i] = dp[j]+1;
+            } 
+        }
+        
+        return *max_element(dp, dp + sizeof(dp)/sizeof(dp[0]));
     }
- 
-    // Return maximum value in lis[]
-    return *max_element(lis, lis + n);    }
 };
