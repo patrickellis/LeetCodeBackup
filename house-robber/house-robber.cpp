c264d090c718f23f6a3344c5e4cb27e4eec703d9
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-04 17:11:50 +0100

    Time: 3 ms (40.74%), Space: 7.7 MB (71.91%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int sz = nums.size();
-        // base cases
-        if(sz==1) return *begin(nums);
-        if(sz==2) return *max_element(nums.begin(),nums.end());
-        // dp array, dp[i] max possible by house i
-        int dp[sz];
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        int n = nums.size();
+        if(n == 1) return nums.back();
+        if(n == 2) return max(nums[0],nums[1]);
         
-        for(int i = 2; i < sz; ++i){
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        int dp[n];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        dp[2] = max(dp[1],nums[2]+dp[0]);
+        for(int i = 3; i < n; ++i){
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);    
         }
-        for(int i=0;i<sz;i++) cout << dp[i] << " ";
-        return max(dp[sz-1],dp[sz-2]);
+        return max(dp[n-1], dp[n-2]);
     }
 };
