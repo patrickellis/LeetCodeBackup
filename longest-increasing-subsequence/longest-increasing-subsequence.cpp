df38de8fd4d5232a6d2112d81044fad4545abbed
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-04 15:20:47 +0100

    Time: 13 ms (70.92%), Space: 10.5 MB (61.45%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(); 
-        int dp[n]; // dp[i] = longest subsequence that ends at index i;
-        for(int i = 0; i < n; ++i) dp[i] = 1;
+        vector<int> LIS;
         
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < i; ++j){
-                if(nums[i] > nums[j] && dp[i] < dp[j]+1)
-                    dp[i] = dp[j]+1;
-            } 
+        for(int i = 0; i < nums.size(); ++i){
+            if(LIS.empty() || LIS.back() < nums[i]){
+                LIS.push_back(nums[i]);
+            }       
+            else{
+                auto it = lower_bound(LIS.begin(), LIS.end(),nums[i]);
+                *it = nums[i];
+            }
         }
-        
-        return *max_element(dp, dp + sizeof(dp)/sizeof(dp[0]));
+        return LIS.size();
     }
 };
