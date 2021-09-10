a7d23b813068f5d8628fd28cd6b9040a70028f29
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-10 14:28:44 +0100

    Time: 3 ms (58.08%), Space: 7.5 MB (15.16%) - LeetHub

--- reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -1,18 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-   ListNode* reverseBetween(ListNode* head, int m, int n) {
-       ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
-       dummy -> next = head;
-       for (int i = 0; i < m - 1; i++) {
-           pre = pre -> next;
-       }
-       cur = pre -> next;
-       for (int i = 0; i < n - m; i++) {
-           ListNode* temp = pre -> next;
-           pre -> next = cur -> next;
-           cur -> next = cur -> next -> next;
-           pre -> next -> next = temp;
-       }
-       return dummy -> next;
-   }
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode* dummy = new ListNode(0), *prev = dummy;
+        dummy->next = head;
+        // position prev pointer to one before start of reversed list        
+        for(int start = 0; start < left-1; ++start)
+            prev = prev->next;
+        ListNode* cur = prev->next;
+        for(int i = 0; i < right-left; ++i){
+            ListNode* temp = prev->next;
+            prev -> next = cur -> next;
+            cur -> next = cur -> next -> next;
+            prev -> next -> next = temp;
+        }
+        return dummy->next;
+    }
 };
