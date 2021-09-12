d14d88519430dfe3220316d1e137eac9491cedb2
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-12 23:18:23 +0100

    Time: 27 ms (32.84%), Space: 17 MB (51.13%) - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        bitset<60001> seen;        
-        for(auto n:nums){
-            seen[n+30000].flip();            
+        int temp = 0;
+        for(auto i:nums){
+            temp ^= i;
         }
-        for(int i = 0; i < 60001; ++i)
-            if(seen[i])
-                return i-30000;
-        return -1;
+        return temp;
     }
 };
