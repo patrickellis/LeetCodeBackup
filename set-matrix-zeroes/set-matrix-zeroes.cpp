7d4685dfcb8e2b86ab09bbc11b176fd0daf614fd
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-13 20:06:07 +0100

    Time: 27 ms (13.14%), Space: 13.2 MB (55.22%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<bool> rows(matrix.size(),false);
+        // track rows & cols with 0's in them
+        vector<bool> rows(matrix.size(), false);
         vector<bool> cols(matrix[0].size(), false);
         for(int i = 0; i < matrix.size(); ++i){
             for(int j = 0; j < matrix[i].size(); ++j){
                 if(matrix[i][j] == 0){
                     rows[i] = true;
                     cols[j] = true;
+                    // update vectors to keep track of
+                    // rows & cols where we  have found
+                    // 0's
                 }
             }
         }
-        for(int r = 0; r < rows.size(); ++r){
-            if(rows[r]){
-                fill(matrix[r].begin(), matrix[r].end(),0);
+        // for each row containing a zero, update all elements
+        // to zero
+        for(int i = 0; i < matrix.size(); ++i){
+            if(rows[i]){
+                for(int j = 0; j <matrix[i].size(); ++j){
+                    matrix[i][j] = 0;
+                }
             }
         }
-        for(int c = 0; c < cols.size(); ++c){
-            if(cols[c]){
+        // for each column containing a zero, update all elements
+        // to zero
+        for(int j = 0; j < matrix[0].size(); j++){
+            if(cols[j]){
                 for(int i = 0; i < matrix.size(); ++i){
-                    matrix[i][c] = 0;    
-                }    
-            }
-        }        
+                    matrix[i][j] = 0;    
+                }
+            }    
+        }
     }
 };
