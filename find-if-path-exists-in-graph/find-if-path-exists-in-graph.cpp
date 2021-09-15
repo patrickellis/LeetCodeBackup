537758f94b15fa6e390ea067676517cc40b65bd1
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-15 21:27:26 +0100

    Time: 1322 ms (8.64%), Space: 145.6 MB (81.41%) - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        vector<vector<int>> graph(n);
-        vector<bool> visited(n, false);
-        // build the graph
-        for(int i = 0; i < edges.size(); ++i){
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
+        vector<int> adj[n];
+        for(auto& e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-        // BFS
-        queue<int> q;
-        q.push(start);
-        visited[start] = true;
-        while(!q.empty()){
-            auto next = q.front();
-            q.pop();
-            if(next == end) 
+        bool visited[n];
+        memset(visited, false, sizeof(visited));
+        // dfs
+        stack<int> s;
+        s.push(start);
+        while(!s.empty()){            
+            auto top = s.top();
+            visited[top] = true;
+            s.pop();
+            if(top == end)
                 return true;
-            else{
-                for(auto e:graph[next]){
-                    if(!visited[e]){
-                        q.push(e);
-                    }
-                }
+            for(auto node : adj[top]){
+                if(visited[node])
+                    continue;
+                s.push(node);
             }
-            visited[next] = true;
         }
+        
         return false;
     }
 };
