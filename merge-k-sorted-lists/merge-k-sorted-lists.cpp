fd56301756390a4b2180a338488ef09cfa0c1993
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-18 15:06:18 +0100

    Time: 24 ms (66.32%), Space: 13.3 MB (74.72%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,35 +9,25 @@
  * };
  */
 class Solution {
-private:
-    void mergeLists(ListNode*& l1, ListNode*& l2){
-        ListNode* head = new ListNode(0,l1);
-        ListNode* temp = head;
-        while(l1 && l2){
-            if(l1->val < l2->val){
-                temp -> next = l1;        
-                l1 = l1 ->next;
-            }
-            else{
-                temp->next = l2;
-                l2 = l2->next;
-            }
-            temp = temp->next;
-        }
-        temp -> next = (l1)?l1:l2;
-        l1 = head->next;
-    }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        if(n == 0) return nullptr;
-        while(n-- > 1){
-            ListNode* &back = lists.back();
-            ListNode* &merge = lists[lists.size()-2];
-            mergeLists(merge,back);
-            lists.pop_back();            
-
+        priority_queue<pair<int,int>> pq;
+        for(int i = 0; i < lists.size(); ++i){
+            if(lists[i] == nullptr)
+                continue;
+            pq.push(make_pair(-lists[i]->val,i));
         }
-        return lists.front();
+        ListNode* res = new ListNode();
+        ListNode* tail = res;
+        while(!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+            tail->next = lists[top.second];
+            lists[top.second] = lists[top.second]->next;
+            if(lists[top.second]!=nullptr)
+                pq.push(make_pair(-lists[top.second]->val,top.second));
+            tail = tail->next;
+        }
+        return res->next;
     }
 };
