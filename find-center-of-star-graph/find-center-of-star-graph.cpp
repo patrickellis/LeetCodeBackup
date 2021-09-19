d100780c56ebd67b6e4dfa6b0ec7a07855e0b8eb
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2021-09-19 10:55:57 +0100

    Time: 300 ms (19.53%), Space: 90.7 MB (10.15%) - LeetHub

--- find-center-of-star-graph/find-center-of-star-graph.cpp
+++ find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        for(auto n:edges.front())
-            if(find(edges.back().begin(), edges.back().end(),n) != edges.back().end())
-                return n;
+        unordered_map<int,int> M;
+        int i = 0;
+        for(auto e:edges){
+            if(M.find(e[0])==M.end()) i++;
+            if(M.find(e[1])==M.end()) i++;
+            M[e[0]]++;
+            M[e[1]]++;
+        }
+        printf("value of i is %d",i);
+        for(auto& [k,v]:M){
+            printf("node %d seen %d times \n",k,v);
+            if(v == i-1) return k;
+        }
         return -1;
     }
 };
